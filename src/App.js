f2577146f37dbedd2af54db9eb50973fc1a79fb1
martypowell <jpowel25@gmail.com>
2020-05-11 13:58:05 -0400

    moved fitlers to their own file, this will probably get big

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import { Config } from "@baltimorecounty/javascript-utilities";
+import { FilterList } from "@baltimorecounty/react-filter-list";
+import ParkFilters from "./Filters";
 import React from "react";
+import { Run } from "./Startup";
+
+const { getValue } = Config;
+
+Run();
 
 function App() {
-  return <div className="App">app goes here</div>;
+  return (
+    <div className="App">
+      <FilterList
+        title="Baltimore County Parks and Facilities"
+        filters={ParkFilters}
+        apiEndpoint={getValue("parksApi")}
+        renderItem={({ name, address, amenities = [] }) => (
+          <div
+            style={{
+              border: "1px solid #e0e0e0",
+              padding: "10px",
+              marginBottom: "10px",
+            }}
+          >
+            <h2>{name}</h2>
+            <p>{address}</p>
+            {amenities.map((x) => (
+              <p>{x.label}</p>
+            ))}
+          </div>
+        )}
+      />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Filters.js
@@ -0,0 +1,22 @@
+const Filters = [
+  {
+    targetApiField: "amenities",
+    displayName: "Amenities",
+    options: [
+      {
+        label: "Accessible Amphitheater",
+        value: "Accessible Amphitheater",
+      },
+      {
+        label: "Accessible Playgrounds",
+        value: "Accessible Playgrounds",
+      },
+      {
+        label: "Horseshoe Pits",
+        value: "Horseshoe Pits",
+      },
+    ],
+  },
+];
+
+export default Filters;
