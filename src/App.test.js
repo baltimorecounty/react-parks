07324318dd29973ca98c27fdbb45bee31075fc4c
martypowell <jpowel25@gmail.com>
2020-05-15 09:27:51 -0400

    added minimum test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import App from "./App";
 import React from "react";
 import { render } from "@testing-library/react";
 
-test("renders learn react link", () => {
+test("renders app", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/app goes here/i);
-  expect(linkElement).toBeInTheDocument();
+  const amenityFilterElement = getByText(/amenities/i);
+  expect(amenityFilterElement).toBeInTheDocument();
 });
